b9349eae7174b97b60add564ddca15df261e2505
rozwer <132114797+rozwer@users.noreply.github.com>
2024-10-25 16:39:16 +0900

    test

--- backend.js
+++ backend.js
@@ -1,9 +1,9 @@
 // backend.js
 import { firestore } from "./firebaseConfig";
 
-export const submitDataToFirestore = async (data) => {
+export const submitDataToFirestore = async (data, collectionName) => {
   try {
-    await firestore.collection("userInputs").add(data);
+    await firestore.collection(collectionName).add(data); // 引数で受け取ったコレクション名を使用
     console.log("データが正常に送信されました");
   } catch (error) {
     console.error("Firestoreへの送信エラー: ", error);
