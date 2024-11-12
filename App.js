61ff350c58ad10f0042ce1cbe9b2ccdae9c5c0d3
gla <rikuto112.3r@gmail.com>
2024-11-12 16:52:06 +0900

    表示できるやつ

--- App.js
+++ App.js
@@ -2,15 +2,23 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import AppNavigator from "./component/messagerouting"; // AppNavigatorのパスを確認
 import LoginWithEmail from "./login";
+import { ChatRoomSelection } from "./component/ChatRoomSelection";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { ChatAdmin } from "./component/chatAdmin";
+import { Chatclub } from "./component/chatClub";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello</Text>
-
-      <AppNavigator />
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="ChatRoomSelection">
+        <Stack.Screen name="ChatRoomSelction" component={ChatRoomSelection}/>
+        <Stack.Screen name="ChatAdmin" component={ChatAdmin}/>
+        <Stack.Screen name="Chatclub" component={Chatclub}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
