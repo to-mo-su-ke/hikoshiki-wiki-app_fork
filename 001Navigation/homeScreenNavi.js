48e288cd7c0015037d2dcb7c5b04ad7347793f08
ruki007 <ru07ki02@outlook.jp>
2025-02-21 14:50:41 +0900

    Add previously ignored files

--- 001Navigation/homeScreenNavi.js
+++ 001Navigation/homeScreenNavi.js
@@ -3,7 +3,9 @@ import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from '../007Pages/homeScreenPages/HomeScreen';
 import InputScreen from '../007Pages/homeScreenPages/InputScreen';
 import NewScreen from '../007Pages/homeScreenPages/Newscreen';
-
+import ClubSearch from '../007Pages/homeScreenPages/Clubsearch';
+import Clubdetail from '../007Pages/homeScreenPages/Clubdetail';
+import Classsearch from '../007Pages/homeScreenPages/Classsearch';
 const Stack = createStackNavigator();
 
 const HomeScreenNavigator = () => {
@@ -11,7 +13,10 @@ const HomeScreenNavigator = () => {
     <Stack.Navigator initialRouteName="Home">
       <Stack.Screen name="Home" component={HomeScreen} />
       <Stack.Screen name="Input" component={InputScreen} />
+      <Stack.Screen name="ClubSearch" component={ClubSearch} />
+      <Stack.Screen name="Clubdetail" component={Clubdetail} />
       <Stack.Screen name="New" component={NewScreen} />
+      <Stack.Screen name="Classsearch" component={Classsearch} />
     </Stack.Navigator>
   );
 };
---
+++ 007Pages/homeScreenPages/gradecheck.tsx
@@ -0,0 +1,91 @@
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
+import { doc, getDoc } from 'firebase/firestore';
+import { db } from '../../006Configs/firebaseConfig';
+
+const Gradecheck = ({ route }) => {
+  const { gradeId } = route.params;
+  const { department } = route.params;
+  const [grade, setgrade] = useState(null);
+  const [userdepartment, setuserdepartment] = useState(null);
+
+
+
+
+  useEffect(() => {
+    const fetchgrade = async () => {
+      try {
+        const gradeDocRef = doc(db, 'grade', gradeId);
+        const gradeDoc = await getDoc(gradeDocRef);
+        if (gradeDoc.exists()) {
+          setgrade(gradeDoc.data());
+        }
+      } catch (error) {
+        console.error('Error fetching your grade: ', error);
+      }
+    };
+
+    fetchgrade();
+  }, [gradeId]);
+  useEffect(() => {
+    const fetchuserdepartment = async () => {
+        try {
+            const userdepartmentDocRef = doc(db, 'user', department);
+            const userdepartmentDoc = await getDoc(userdepartmentDocRef);
+            if (userdepartmentDoc.exists()) {
+                setuserdepartment(userdepartmentDoc.data());
+            }
+            }
+        catch (error) {
+            console.error('Error fetching user department: ', error);
+        }
+    
+    }
+        fetchuserdepartment();
+    }   
+    , [userdepartment]);
+
+
+
+  
+
+  if (!grade) {
+    return (
+      <View style={styles.container}>
+        <Text>Loading...</Text>
+      </View>
+    );
+  }
+
+  return (
+    <ScrollView>
+    <View style={styles.container}>
+      
+      <Text> あなたのGPA:{grade.gpaaverage} </Text>
+        <Text> あなたの単位数:{grade.units} </Text>
+        <Text> 卒業まであと:{grade.remainunits}単位 </Text>
+    {userdepartment && grade.department && grade.department[userdepartment.name] && (
+        <Text>学科に対応する数値: {grade.department[userdepartment.name]}</Text>
+    )}
+        
+      
+    </View>
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  clubName: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 16,
+  },
+});
+
+export default Gradecheck;
