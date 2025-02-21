c184f721164c003dcaa0d11922ba244a457e9714
rozwer <132114797+rozwer@users.noreply.github.com>
2025-02-21 17:09:47 +0900

    調整

--- 001Navigation/homeScreenNavi.js
+++ 001Navigation/homeScreenNavi.js
@@ -3,9 +3,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from '../007Pages/homeScreenPages/HomeScreen';
 import InputScreen from '../007Pages/homeScreenPages/InputScreen';
 import NewScreen from '../007Pages/homeScreenPages/Newscreen';
-import ClubSearch from '../007Pages/homeScreenPages/Clubsearch';
-import Clubdetail from '../007Pages/homeScreenPages/Clubdetail';
-import Classsearch from '../007Pages/homeScreenPages/Classsearch';
+
 const Stack = createStackNavigator();
 
 const HomeScreenNavigator = () => {
@@ -13,10 +11,7 @@ const HomeScreenNavigator = () => {
     <Stack.Navigator initialRouteName="Home">
       <Stack.Screen name="Home" component={HomeScreen} />
       <Stack.Screen name="Input" component={InputScreen} />
-      <Stack.Screen name="ClubSearch" component={ClubSearch} />
-      <Stack.Screen name="Clubdetail" component={Clubdetail} />
       <Stack.Screen name="New" component={NewScreen} />
-      <Stack.Screen name="Classsearch" component={Classsearch} />
     </Stack.Navigator>
   );
 };
---
+++ 006Configs/firebaseConfig.js
@@ -0,0 +1,11 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDBUTuJaOcEdQvT2jAaoEdZV52IPQDKqH4",
+    authDomain: "unofficialwikigroup4.firebaseapp.com",
+    databaseURL:
+      "https://unofficialwikigroup4-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "unofficialwikigroup4",
+    storageBucket: "unofficialwikigroup4.appspot.com",
+    messagingSenderId: "942892902022",
+    appId: "1:942892902022:web:d0432a88360497a12e1307",
+    measurementId: "G-CGSGNBQ2GD",
+  };
